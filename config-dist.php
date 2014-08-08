26d209eb21d8fcc9851e9d5739397c6489f876da
Romans Malinovskis <romans@agiletoolkit.org>
2014-08-08 11:50:32 +0100

    fix

--- config-dist.php
+++ config-dist.php
@@ -1,4 +1,4 @@
 <?php
 require_once'config-default.php';
-$config['dsn']=str_replace('mysql2','mysql',$_ENV['DATABASE_URL'];
+$config['dsn']=str_replace('mysql2','mysql',$_ENV['DATABASE_URL']);
 var_Dump($_ENV);
