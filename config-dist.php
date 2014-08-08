603f829f99abc367ef7bc7937751c17cec7059e6
Romans Malinovskis <romans@agiletoolkit.org>
2014-08-08 11:58:50 +0100

    include pest

--- config-dist.php
+++ config-dist.php
@@ -1,3 +1,6 @@
 <?php
 require_once'config-default.php';
-$config['dsn']=str_replace('mysql2','mysql',$_ENV['DATABASE_URL']);
+$config['dsn']=str_replace(
+  ['mysql2',':3306'],
+  ['mysql',''],
+  $_ENV['DATABASE_URL']);
